8b7813a5088a1f8540f39b7cc285d87b3665f305
RubenJ99 <juarezperezruben13@gmail.com>
2021-12-19 17:10:29 +0100

    estruct

---
+++ Practica 4/js/List.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function List(data){
+    if(!this instanceof List) throw new Error("CUSTOM ERROR"); 
+    if(typeof(data) === "object") throw new Error("CUSTOM ERROR");
+    
+    let _storage = [];
+    
+
+}
